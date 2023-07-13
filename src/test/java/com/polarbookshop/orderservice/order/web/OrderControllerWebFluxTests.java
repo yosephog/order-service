0447841a592efa9358ad11c30e5a3cbf3696a5c8
yosephog <thorfry@yahoo.com>
2023-07-13 12:46:14 +0200

    order service with ci cd

--- src/test/java/com/polarbookshop/orderservice/order/web/OrderControllerWebFluxTests.java
+++ src/test/java/com/polarbookshop/orderservice/order/web/OrderControllerWebFluxTests.java
@@ -4,17 +4,19 @@ import com.polarbookshop.orderservice.order.domain.Order;
 import com.polarbookshop.orderservice.order.domain.OrderService;
 import com.polarbookshop.orderservice.order.domain.OrderStatus;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import reactor.core.publisher.Mono;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 
 @WebFluxTest(OrderController.class)
-public class OrderControllerWebFluxTests {
+class OrderControllerWebFluxTests {
+
     @Autowired
     private WebTestClient webClient;
 
@@ -24,15 +26,13 @@ public class OrderControllerWebFluxTests {
     @Test
     void whenBookNotAvailableThenRejectOrder() {
         var orderRequest = new OrderRequest("1234567890", 3);
-        var expectedOrder = OrderService.buildRejectedOrder(
-                orderRequest.isbn(), orderRequest.quantity());
-        given(orderService.submitOrder(
-                orderRequest.isbn(), orderRequest.quantity())
-        ).willReturn(Mono.just(expectedOrder));
+        var expectedOrder = OrderService.buildRejectedOrder(orderRequest.isbn(), orderRequest.quantity());
+        given(orderService.submitOrder(orderRequest.isbn(), orderRequest.quantity()))
+                .willReturn(Mono.just(expectedOrder));
 
         webClient
                 .post()
-                .uri("/orders/")
+                .uri("/orders")
                 .bodyValue(orderRequest)
                 .exchange()
                 .expectStatus().is2xxSuccessful()
@@ -40,5 +40,7 @@ public class OrderControllerWebFluxTests {
                     assertThat(actualOrder).isNotNull();
                     assertThat(actualOrder.status()).isEqualTo(OrderStatus.REJECTED);
                 });
+
     }
+
 }
